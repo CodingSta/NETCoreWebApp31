1bae452fd2860c9e182026de9d3ba7b2e61fc2f1
MinGyu, Ju <knighp@gmail.com>
2020-08-03 09:48:31 +0900

    2nd commit

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace NETCoreWebApp31
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello MinGyu!");
+                    await context.Response.WriteAsync("Hello RecursiveSoft!");
                 });
             });
         }
